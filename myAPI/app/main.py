59eeb1a6c779a0463e574295b4501f4f5f0377cf
PolisTP98 <123047038@upq.edu.mx>
2026-02-25 02:44:26 -0600

    Update main.py

--- myAPI/app/main.py
+++ myAPI/app/main.py
@@ -32,10 +32,10 @@ users = [
 # -----------------------------------
 
 class UserBase(BaseModel):
-    id: int = Field(..., gt = 0, description = "User identifier", example = 1)
-    name: str = Field(..., min_length = 3, max_length = 50, description = "User name")
-    age: int = Field(..., ge = 0, le = 121, description = "Valid age between 0 and 121")
-    aka: str = Field(..., min_length = 3, max_length = 50, description = "User nickname", example = "The GOAT")   
+    id: int = Field(..., gt = 0, description = "User identifier", examples = [1])
+    name: str = Field(..., min_length = 3, max_length = 255, description = "User name", examples = ["Isaac Abdiel Sánchez López"])
+    age: int = Field(..., ge = 0, le = 121, description = "Valid age between 0 and 121", examples = [20])
+    aka: str = Field(..., min_length = 3, max_length = 50, description = "User nickname", examples = ["The GOAT"])
 
 
 # -------------
