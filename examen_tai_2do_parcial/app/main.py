dade88c5a5523c717ca798f4c4a8a88e3efc8bb8
PolisTP98 <123047038@upq.edu.mx>
2026-03-09 18:50:39 -0600

    Segundo commit de la API del examen de segundo parcial TAI

--- examen_tai_2do_parcial/app/main.py
+++ examen_tai_2do_parcial/app/main.py
@@ -13,6 +13,9 @@ app = FastAPI(
     description = "Isaac Abdiel Sánchez López", 
 )
 
+id_artificial = 1
+lista_de_citas = []
+
 pacientes = [
     {"id_paciente": 1, "name": "Daniela Lisset Elizalde Ortiz", "age": 20}, 
     {"id_paciente": 2, "name": "Gabriela Martínez Cruz", "age": 22}, 
@@ -22,7 +25,7 @@ pacientes = [
 
 security = HTTPBasic()
 
-def verify_request(credentials: HTTPBasicCredentials = Depends(security)):
+def validar_credenciales(credentials: HTTPBasicCredentials = Depends(security)):
     usuario = secrets.compare_digest(credentials.username, "root")
     contrasena = secrets.compare_digest(credentials.password, "1234")
 
@@ -34,8 +37,7 @@ def verify_request(credentials: HTTPBasicCredentials = Depends(security)):
     return credentials.username
 
 class CrearCita(BaseModel):
-    id_cita: int = Field(..., gt = 0, description = "Indentificador de la cita", examples = [1])
-    nombre_paciente: str = Field(..., min_length = 5, max_length = 255, description = "Nombre del paciente", examples = ["Isaac Abdiel Sánchez López"])
+    id_paciente: int = Field(gt = 1, ge = len(pacientes))
     fecha: datetime
     motivo: Optional[str] = None
 
@@ -43,11 +45,9 @@ class ConfirmarCita(CrearCita):
     id_cita: int
     confirmada: bool
 
-class EliminarCita(CrearCita):
-    id_cita: int
-    disponible: bool
-
-lista_de_citas = []
+def generar_id_unico():
+    id_artificial += 1 # type: ignore
+    return id_artificial
 
 def encontrar_cita(id_cita):
     for cita in lista_de_citas:
@@ -59,11 +59,6 @@ def cita_confirmada(id_cita):
     for cita in lista_de_citas:
         if cita["id_cita"] == id_cita:
             return cita["confirmada"]
-    
-def cita_eliminada(id_cita):
-    for cita in lista_de_citas:
-        if cita["id_cita"] == id_cita:
-            return cita["disponible"]
 
 @app.get("/comprobar_conexion", tags = ["HEALTH_CHECK"])
 async def comprobar_conexion():
@@ -72,3 +67,69 @@ async def comprobar_conexion():
         "status": "200"
     }
 
+@app.post("/v1/citas", status_code = status.HTTP_201_CREATED)
+async def crear_cita(list_index: int, cita: CrearCita):
+    nueva_cita = {
+        "id_cita": generar_id_unico(), 
+        "id_paciente": pacientes[0]["id_paciente"], 
+        "fecha": cita.fecha, 
+        "motivo": cita.motivo
+    }
+
+    lista_de_citas.append(nueva_cita)
+
+    if nueva_cita["fecha"] >= datetime.now():
+        return ConfirmarCita(
+            id_cita = nueva_cita["id_cita"], 
+            id_paciente = pacientes[list_index]["id_paciente"], 
+            fecha = nueva_cita["fecha"], 
+            motivo = nueva_cita["motivo"], 
+            confirmada = False
+        )
+    
+    raise HTTPException(
+        status_code = status.HTTP_409_CONFLICT,
+        detail = "La fecha de la cita no puede ser menor a la actual"
+    )
+
+@app.get("/v1/citas/{id}", tags = ["CITAS"])
+async def obtener_cita(id_cita: int):
+    cita = encontrar_cita(id_cita)
+    if not cita:
+        raise HTTPException(
+            status_code = 404, 
+            detail = "Cita no encontrada"
+        )
+    return {"Cita encontrada": cita}
+
+@app.get("/v1/citas", tags = ["CITAS"])
+async def obtener_citas(usuario: str = Depends(validar_credenciales)):
+    return {
+        "status": "200", 
+        "total": len(lista_de_citas), 
+        "data": lista_de_citas
+    }
+
+@app.put("/v1/citas", tags = ["CITAS"], status_code = status.HTTP_200_OK)
+async def confirmar_cita(id_cita: int):
+    cita = encontrar_cita(id_cita)
+    if not cita:
+        raise HTTPException(
+            status_code = 404, 
+            detail = "Cita no encontrada"
+        )
+    
+    cita["confirmada"] = True
+    return ConfirmarCita(**cita)
+
+@app.delete("/v1/citas", tags = ["CITAS"], status_code = status.HTTP_204_NO_CONTENT)
+async def eliminar_cita(id_cita: int, usuario: str = Depends(validar_credenciales)):
+    for index, cita in enumerate(lista_de_citas):
+        if cita["id_cita"] == id:
+            lista_de_citas.pop(index)
+            return
+            
+    raise HTTPException(
+        status_code = status.HTTP_404_NOT_FOUND, 
+        detail = f"No se encontró la cita con ID: {id_cita}"
+    )
